4de10c810e449d402161e12f7c31be9ee9d88bf1
Shurinai-stack <kanybekovislam08@gmail.com>
2025-11-04 15:22:34 +0600

    hw2 added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=1000, null=True, blank=True)
+    rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.content} - {self.rate} - {self.created_at} - {self.updated_at}"
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render,HttpResponse
+from posts.models import Post
+
+def home_view(request):
+    return render(request, "base.html")
 
 def test_view(request):
     return HttpResponse("Криштиану Роналду Душ Сантос Авейру")
 
 def view_html(request):
     return render(request, "base.html")
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "posts/posts_list.html", context={"posts":posts})
